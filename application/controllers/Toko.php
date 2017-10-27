ef5dff639886da0d87bdd23f34324977b9cdd5a3
Neo Alit Cahya <neo.alit.cahya@live.com>
2017-10-27 15:52:28 +0700

    1. tambah toko

--- application/controllers/Toko.php
+++ application/controllers/Toko.php
@@ -57,4 +57,103 @@ class Toko extends CI_Controller {
 		$this->load->view('tambahToko',$data);
 		//$this->load->view('footer');
 	}
+
+	public function prosesTambahToko()
+	{
+		$this->load->helper(array('form', 'url'));
+		$this->load->library('form_validation');
+
+		//isi data diisi array post
+		$isiData = $this->input->post();
+		//print_r($isiData);
+		$isLow=FALSE;
+
+		if($this->input->post('btnTambah'))
+		{
+			$this->form_validation->set_rules('kodeToko', 'Kode toko', 'required');
+			$this->form_validation->set_rules('namaToko', 'Nama toko', 'required');
+			$this->form_validation->set_rules('contactPersonToko', 'Contact person', 'required');
+			$this->form_validation->set_rules('alamatEmailToko', 'Email Toko', 'required');
+			$this->form_validation->set_rules('alamatToko', 'Nama Toko', 'required');
+			$this->form_validation->set_rules('pilihKotaToko', 'Pilih kota', 'required');
+			$this->form_validation->set_rules('kodePosToko', 'Kode Pos', 'required');
+			$this->form_validation->set_rules('teleponToko', 'Telepon', 'required');
+			$this->form_validation->set_rules('hpToko', 'Hp', 'required');
+			$this->form_validation->set_rules('faximileToko', 'Faximile', 'required');
+			$this->form_validation->set_rules('limitPiutangToko', 'Limit piutang', 'required');
+			$this->form_validation->set_rules('jatuhTempoToko', 'Jatuh tempo', 'required');
+			
+			if ($this->form_validation->run() == FALSE)
+			{
+				echo "Ada yang belum anda isi";
+           	}
+           	else
+           	{
+				$kodeToko	= $this->input->post('kodeToko');
+				$namaToko	= $this->input->post('namaToko');
+				$contactPersonToko	= $this->input->post('contactPersonToko');
+				$alamatEmailToko	= $this->input->post('alamatEmailToko');
+				$pilihKotaToko	= $this->input->post('pilihKotaToko');
+				$kodePosToko	= $this->input->post('kodePosToko');
+				$teleponToko	= $this->input->post('teleponToko');
+				$hpToko	= $this->input->post('hpToko');
+				$faximileToko	= $this->input->post('faximileToko');
+				$limitPiutangToko	= $this->input->post('limitPiutangToko');
+				$jatuhTempoToko	= $this->input->post('jatuhTempoToko');
+
+				$result = $this->Toko_Model->insert_toko($kodeToko, $alamatEmailToko, $namaToko, $contactPersonToko, $alamatToko, $kodePosToko, $teleponToko, $hpToko, $faximileToko, $limitPiutangToko, $jatuhTempoToko, $pilihKotaToko);
+
+				/*
+
+				if(count($result) > 0)
+				{
+					$dataMenu = array(
+				        'menuAktif' => "merk"
+					);
+
+					$dataMerk = $this->Merk_Model->get_allMerk();
+
+					$dataInfo = array(
+						//status 1 berarti success
+				        'status' => "1",
+				        'keterangan' => "Merk baru berhasil ditambahkan",
+					);
+					$data = array(
+				        'dataMerk' => $dataMerk,
+				        'dataInfo' => $dataInfo
+					);
+                	$this->load->view('header');
+					$this->load->view('sidebar',$dataMenu);
+					$this->load->view('merk',$data);
+				} 
+				else 
+				{
+					 $dataMenu = array(
+				        'menuAktif' => "merk"
+					);
+
+					$dataMerk = $this->Merk_Model->get_allMerk();
+
+					$dataInfo = array(
+						//status 0 berarti gagal
+				        'status' => "0",
+				        'keterangan' => "Tidak berhasil dalam menambahkan merk baru",
+					);
+					$data = array(
+				        'dataMerk' => $dataMerk,
+				        'dataInfo' => $dataInfo
+					);
+                	$this->load->view('header');
+					$this->load->view('sidebar',$dataMenu);
+					$this->load->view('merk',$data);
+				}
+				*/
+         	}
+		}
+		else
+		{
+			echo "jangan lakukan refresh saat pengiriman data";
+			redirect(base_url()."merk", 'refresh');
+		}
+	}
 }
